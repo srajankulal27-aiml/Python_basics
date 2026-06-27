eb81afb3804fd2b6e01acb2229e6b460601760f6
Srajan M Kulal <srajanaiml27@gmail.com>
2026-06-27 13:57:17 +0530

    git commands

---
+++ test.py
@@ -0,0 +1,26 @@
+#print("Hello, World!","World",sep="#")
+
+
+
+#x="10.5"
+#y="s"
+#print("abc">"abd")
+#print(int(float(x)))
+#print(float("10.5") + 5)
+#print(r"\test\ntesting")
+#print("\\test\\ntesting")
+#print("\test\ntesting")
+#print("""hello 
+#      my name is 
+#      john""")
+
+#x="hello"
+#print(x[0:3:2])# start:end:step
+#print(x[::-1])#reverse string
+
+#for i in range(1,10):
+#    if i%2==0:
+#        print(i)
+
+for i in range(2,10,2):
+    print(i)
